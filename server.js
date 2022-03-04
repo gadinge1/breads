9d8392f33c78a0f85e5fa6c7451234d19c91aca7
Ella Gading <ellagading@ellas-mbp.myfiosgateway.com>
2022-03-03 20:00:35 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express()
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -23,5 +24,8 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log('nomming at port', PORT);
 })
-
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
  
